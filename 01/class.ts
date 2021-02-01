acb72227c369f3ac91488900bd073b163f52eed3
liuning108 <122273014@qq.com>
2021-02-01 17:42:07 +0800

    update class.ts

--- 01/class.ts
+++ 01/class.ts
@@ -1,5 +1,9 @@
 class Animal {
-    name:string
+    public name:string //default is public (public ,private,protected)
+    static categoies :string [] =['mammal','bird']
+    static isAnimal(a){
+        return a instanceof Animal
+    }
     constructor(name:string) {
         this.name = name
     }
@@ -10,6 +14,8 @@ class Animal {
 
 const snake = new Animal("Lily")
 console.log(snake.run())
+console.log(Animal.categoies)
+console.log(Animal.isAnimal(snake))
 
 
 class Dog extends  Animal {
