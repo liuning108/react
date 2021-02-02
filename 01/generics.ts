725ce25287b44f338959bc4ff77bfd184a47fb0e
liuning <122273014@qq.com>
2021-02-03 06:20:55 +0800

    add generics.ts

--- 01/generics.ts
+++ 01/generics.ts
@@ -1,3 +1,5 @@
+/// <reference path = "../jQuery.d.ts" />
+
 function echo<T>(arg:T):T{
     return arg
 }
@@ -26,3 +28,39 @@ const obj2 = echoWithLength([1,2,3])
 const obj3 = echoWithLength({length:10})
 
 
+
+class Queue<T> {
+    private  data  =[]
+    push(item:T){
+        return this.data.push(item)
+    }
+    pop():T{
+        return this.data.shift()
+    }
+
+}
+const q = new Queue<number>()
+q.push(1)
+q.push(2)
+console.log(q.pop().toFixed())
+console.log(q.pop().toFixed())
+
+
+interface KeyPair<T,U> {
+    key :T
+    value:U
+}
+let kp1:KeyPair<number, string> = {
+    key:1,
+    value:"12"
+}
+let arr: number[] = [1,2,3]
+let arr2: Array<number> = [4,5,6]
+
+interface IPlus<T>{
+    (a:T,b:T):T
+}
+function plus(a:number,b:number):number{
+    return  a+b
+}
+const a:IPlus<number> =plus
